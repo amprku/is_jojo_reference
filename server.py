8bd8017aea1b623c45605953e45bc23784337e2a
Amit Kulkarni <am.pr.ku@gmail.com>
2020-05-22 17:39:24 -0700

    added flask cors.

--- server.py
+++ server.py
@@ -3,9 +3,10 @@
 import os
 from flask import Flask, render_template, request
 import json
+from flask_cors import CORS
 # import compile_data
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def index():
